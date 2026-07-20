63f323372f428a97129b7105463634b935141bb7
BereanK <bereankibet@gmail.com>
2026-07-20 10:34:13 +0300

    Bug fixes on search and widgets

--- app/Filament/Resources/LogbookProfiles/Widgets/LogbookStatOverView.php
+++ app/Filament/Resources/LogbookProfiles/Widgets/LogbookStatOverView.php
@@ -28,19 +28,37 @@ class LogbookStatOverView extends StatsOverviewWidget
 
             Stat::make('Pending', number_format($this->getPageTableQuery()->where('status', LogBookStatusEnum::PENDING->value)->count()))
                 ->descriptionIcon('heroicon-m-paper-airplane')
+                ->url(route('filament.admin.resources.logbook-profiles.index', [
+                    'filters[Status][values][0]' => 1
+                ]))
+                ->openUrlInNewTab()
                 ->description('Pednding')
                 ->color(LogBookStatusEnum::PENDING->color()),
             Stat::make('Processing', number_format($this->getPageTableQuery()->where('status', LogBookStatusEnum::PROCESSING->value)->count()))
                 ->descriptionIcon('heroicon-m-arrow-path-rounded-square')
                 ->description('Processing')
+
+                ->url(route('filament.admin.resources.logbook-profiles.index', [
+                    'filters[Status][values][0]' => LogBookStatusEnum::PROCESSING->value
+                ]))
+                ->openUrlInNewTab()
                 ->color(LogBookStatusEnum::PROCESSING->color()),
             Stat::make('Pending Acceptance', number_format($this->getPageTableQuery()->where('status', LogBookStatusEnum::PENDING_ACCEPTANCE->value)->count()))
                 ->descriptionIcon('heroicon-m-clock')
+                ->description('Processing')
+                ->url(route('filament.admin.resources.logbook-profiles.index', [
+                    'filters[Status][values][0]' => LogBookStatusEnum::PENDING_ACCEPTANCE->value
+                ]))
+                 ->openUrlInNewTab()
                 ->description('Pending Acceptance')
                 ->color(LogBookStatusEnum::PENDING_ACCEPTANCE->color()),
             Stat::make('With Issues', number_format($this->getPageTableQuery()->where('status', LogBookStatusEnum::WITH_ISSUES->value)->count()))
                 ->descriptionIcon('heroicon-m-x-circle')
                 ->description('With Issues')
+                ->url(route('filament.admin.resources.logbook-profiles.index', [
+                    'filters[Status][values][0]' => LogBookStatusEnum::WITH_ISSUES->value
+                ]))
+                ->description('Pending Acceptance')
                 ->color(LogBookStatusEnum::WITH_ISSUES->color()),
         ];
     }
--- app/Filament/Widgets/LogbookOverviewWidget.php
+++ app/Filament/Widgets/LogbookOverviewWidget.php
@@ -17,13 +17,26 @@ class LogbookOverviewWidget extends StatsOverviewWidget
             Stat::make('Pending', number_format($logbookQuery->where('status', LogBookStatusEnum::PENDING->value)->count()))
                 ->descriptionIcon('heroicon-m-paper-airplane')
                 ->description('Pednding')
+                ->url(route('filament.admin.resources.logbook-profiles.index', [
+                    'filters[Status][values][0]' => 1
+                ]))
+                ->openUrlInNewTab()
+                ->description('Pednding')
                 ->color(LogBookStatusEnum::PENDING->color()),
             Stat::make('Processing', number_format(LogbookProfile::where('status', LogBookStatusEnum::PROCESSING->value)->count()))
                 ->descriptionIcon('heroicon-m-arrow-path-rounded-square')
                 ->description('Processing')
+                ->url(route('filament.admin.resources.logbook-profiles.index', [
+                    'filters[Status][values][0]' => LogBookStatusEnum::PROCESSING->value
+                ]))
+                ->openUrlInNewTab()
                 ->color(LogBookStatusEnum::PROCESSING->color()),
             Stat::make('Pending Acceptance', number_format(LogbookProfile::where('status', LogBookStatusEnum::PENDING_ACCEPTANCE->value)->count()))
                 ->descriptionIcon('heroicon-m-clock')
+                ->url(route('filament.admin.resources.logbook-profiles.index', [
+                    'filters[Status][values][0]' => LogBookStatusEnum::PENDING_ACCEPTANCE->value
+                ]))
+                 ->openUrlInNewTab()
                 ->description('Pending Acceptance')
                 ->color(LogBookStatusEnum::PENDING_ACCEPTANCE->color()),
             Stat::make('With Issues', number_format(LogbookProfile::where('status', LogBookStatusEnum::WITH_ISSUES->value)->count()))
